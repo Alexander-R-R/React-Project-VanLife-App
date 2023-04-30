bc70392475bef916543571295dfe14c624fb43fe
Alexander-R-R <a.roussev@yahoo.com>
2023-04-30 19:08:21 +0300

    updates

--- src/pages/Vans/Vans.js
+++ src/pages/Vans/Vans.js
@@ -35,22 +35,47 @@ export default function Vans() {
     </div>
   ));
 
+  function handleFilterChange(key, value) {
+    setSearchParams((prevParams) => {
+      if (value === null) {
+        prevParams.delete(key);
+      } else {
+        prevParams.set(key, value);
+      }
+      return prevParams;
+    });
+  }
+
   return (
     <div className="van-list-container">
       <h1>Explore our van options</h1>
       <div className="van-list-filter-buttons">
-        <Link to="?type=simple" className="van-type simple">
+        <button
+          onClick={() => handleFilterChange("type", "simple")}
+          className="van-type simple"
+        >
           Simple
-        </Link>
-        <Link to="?type=rugged" className="van-type rugged">
+        </button>
+        <button
+          onClick={() => handleFilterChange("type", "rugged")}
+          className="van-type rugged"
+        >
           Rugged
-        </Link>
-        <Link to="?type=luxury" className="van-type luxury">
+        </button>
+        <button
+          onClick={() => handleFilterChange("type", "luxury")}
+          className="van-type luxury"
+        >
           Luxury
-        </Link>
-        <Link to="." className="van-type clear-filters">
-          Clear filter
-        </Link>
+        </button>
+        {typeFilter ? (
+          <button
+            onClick={() => handleFilterChange("type", null)}
+            className="van-type clear-filters"
+          >
+            Clear filter
+          </button>
+        ) : null}
       </div>
       <div className="van-list">{vansElements}</div>
     </div>
