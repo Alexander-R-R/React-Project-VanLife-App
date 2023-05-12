bad863592ad384a8a0c507e316505ea59718ff06
Alexander-R-R <a.roussev@yahoo.com>
2023-05-12 19:43:15 +0300

    updates

--- src/api.js
+++ src/api.js
@@ -1,17 +1,57 @@
+import { initializeApp } from "firebase/app"
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite"
 
-export async function getVans(id) {
-  const url = id ? `/api/vans/${id}` : "/api/vans"
-  const res = await fetch(url);
-  if (!res.ok) {
-    throw {
-      message: "Failed to fetch vans",
-      statusText: res.statusText,
-      status: res.status
-    };
-  }
-  const data = await res.json();
-  return data.vans;
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBzseG3GEVLa48HOupxdpjSMWELQCQkxl0",
+  authDomain: "alexander-r---vanlife.firebaseapp.com",
+  projectId: "alexander-r---vanlife",
+  storageBucket: "alexander-r---vanlife.appspot.com",
+  messagingSenderId: "774181499876",
+  appId: "1:774181499876:web:d43168fd73ef1d13e28df1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+const vansCollectionRef = collection(db, "vans")
+
+export async function getVans() {
+  const querySnapshot = await getDocs(vansCollectionRef)
+  const dataArr = querySnapshot.docs.map(doc => ({
+    ...doc.data(),
+    id: doc.id
+  }))
+  console.log(dataArr)
+  return dataArr
 }
+
+
+
+
+
+
+
+
+
+// export async function getVans(id) {
+//   const url = id ? `/api/vans/${id}` : "/api/vans"
+//   const res = await fetch(url);
+//   if (!res.ok) {
+//     throw {
+//       message: "Failed to fetch vans",
+//       statusText: res.statusText,
+//       status: res.status
+//     };
+//   }
+//   const data = await res.json();
+//   return data.vans;
+// }
 
 export async function getHostVans(id) {
   const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
